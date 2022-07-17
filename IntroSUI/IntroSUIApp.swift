8e30528c0c2e486e01739738321ac3972b28dff4
Ahmed Kassem <ahmedkassem996@gmail.com>
2022-07-17 12:02:26 +0200

    Initial Commit

---
+++ IntroSUI/IntroSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSUIApp.swift
+//  IntroSUI
+//
+//  Created by Ahmed Kasem on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct IntroSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
